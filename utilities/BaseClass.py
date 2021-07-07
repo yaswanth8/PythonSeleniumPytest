6a6bc00ece4868266a7971b8254c463727e14259
yaswanth <yaswanthk722@gmail.com>
2021-07-07 18:40:59 +0530

    27.06.2021 - Afternoon

--- utilities/BaseClass.py
+++ utilities/BaseClass.py
@@ -7,7 +7,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 
-
 @pytest.mark.usefixtures("setup")
 class BaseClass:
 
@@ -17,9 +16,7 @@ class BaseClass:
         fileHandler = logging.FileHandler('logfile.log')
         formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s :%(message)s")
         fileHandler.setFormatter(formatter)
-
         logger.addHandler(fileHandler)  # filehandler object
-
         logger.setLevel(logging.DEBUG)
         return logger
 
