87938581e3866f3e57f8222715707082da2f0884
yaswanth <yaswanthk722@gmail.com>
2021-07-07 18:41:29 +0530

    Test 07.07

--- utilities/BaseClass.py
+++ utilities/BaseClass.py
@@ -10,6 +10,7 @@ from selenium.webdriver.support.select import Select
 @pytest.mark.usefixtures("setup")
 class BaseClass:
 
+
     def getLogger(self):
         loggerName = inspect.stack()[1][3]
         logger = logging.getLogger(loggerName)
